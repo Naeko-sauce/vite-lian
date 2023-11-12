a458125a13b0d7a9573f9e12188272afd2b16a90
Naeko-sauce <3443471283@qq.com>
2023-11-12 16:48:46 +0800

    物体的缩放与旋转
    局部物体缩放与旋转，子级的旋转和缩放都和父级有关系

--- chao/src/page/practice/ce.vue
+++ chao/src/page/practice/ce.vue
@@ -1,75 +1,84 @@
 <script setup>
 //导入tjs
-import * as THREE from 'three'
+import * as THREE from "three";
 //导入轨道控制器
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 //创建场景
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 //创建相机
 const camera = new THREE.PerspectiveCamera(
     //视角
     45,
-    window.innerWidth / window.innerHeight//宽高比按照窗口的宽高设置
+    window.innerWidth / window.innerHeight, //宽高比按照窗口的宽高设置
     //近平面
-    , 0.1,
+    0.1,
     //远平面
-    1000
-)
+    1000,
+);
 
 //添加世界坐标辅助器
-const axesHelper = new THREE.AxesHelper(5)
-scene.add(axesHelper)
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
 //创造渲染器
-const renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer();
 //设置渲染尺寸大小
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)//将渲染器挂载到body上
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement); //将渲染器挂载到body上
 //创建几何体
-const geometry = new THREE.BoxGeometry(1, 1, 1)
+const geometry = new THREE.BoxGeometry(1, 1, 1);
 //创建材质
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const ParentCbeMateraial = new THREE.MeshBasicMaterial({ color: 0x0bfff })
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+//父元素材质
+const ParentCbeMateraial = new THREE.MeshBasicMaterial({ color: 0x0bfff });
 //创建父元素
-const parentCbe = new THREE.Mesh(geometry, ParentCbeMateraial)
+const parentCbe = new THREE.Mesh(geometry, ParentCbeMateraial);
+//父元素放大
+parentCbe.scale.set(1, 1, 1);
 //创建网格，就是物体
-const cube = new THREE.Mesh(geometry, material)
+const cube = new THREE.Mesh(geometry, material);
 //将子元素添加到父元素中
-parentCbe.add(cube)
+parentCbe.add(cube);
+//设置立方体的放大,相对于父元素放大，如果父元素放大了子元素也会相应的变化
+cube.scale.set(1, 1, 1);
+//设置绕着x轴旋转,旋转也是跟随着父元素
+cube.rotation.x = Math.PI / 4;
+//设置父元素旋转
+parentCbe.rotation.x = Math.PI / 4;
 //设置网格位置
-cube.position.set(3, 0, 0)
-parentCbe.position.set(-3, 0, 0)
+cube.position.set(3, 0, 0);
+parentCbe.position.set(-3, 0, 0);
 //将网格添加到场景中,更改为添加父元素
-scene.add(parentCbe)
+scene.add(parentCbe);
 //设置相机位置
-camera.position.z = 5
-camera.position.x = 2
-camera.position.y = 2
+camera.position.z = 5;
+camera.position.x = 2;
+camera.position.y = 2;
 //相机看向那里
-camera.lookAt(0, 0, 0)
+camera.lookAt(0, 0, 0);
 //创建轨道控制器
-const controls = new OrbitControls(camera, renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement);
 //设置阻尼
-controls.enableDamping = true
+controls.enableDamping = true;
 //设置阻尼参数
-controls.dampingFactor = 0.05
+controls.dampingFactor = 0.05;
 //渲染函数
 function animate() {
     controls.update();
-    requestAnimationFrame(animate)
+    requestAnimationFrame(animate);
     //x,y,z旋转角度
     // cube.rotation.x += 0.01
     // cube.rotation.y += 0.01
-    renderer.render(scene, camera)
+    renderer.render(scene, camera);
 }
-animate()
+animate();
 //渲染
-renderer.render(scene, camera)
+renderer.render(scene, camera);
 </script>
 
 <template>
     <a href="../login/lgin.vue" target="login">跳转到目标路由</a>
-    <div> <router-link to="/">跳回</router-link></div>
+    <div><router-link to="/">跳回</router-link></div>
 </template>
 
 <style scoped lang="scss">
@@ -80,6 +89,5 @@ canvas {
     left: 0;
     width: 100vw;
     height: 100vh;
-
 }
 </style>
